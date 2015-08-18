b8a2ada3c9e8602b8133e3a7452febf5f0441222
Harshavardhana <harsha@minio.io>
2015-08-18 00:16:59 -0700

    Remove unnecessary usings

--- Minio/V4Authenticator.cs
+++ Minio/V4Authenticator.cs
@@ -18,10 +18,7 @@ using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Minio
 {
