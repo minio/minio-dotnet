ade89f78a84d198a74dc022c9ec600da119eaaf5
Harshavardhana <harsha@harshavardhana.net>
2015-10-25 21:52:01 -0700

    Implement PresignedPostPolicy()

---
+++ Minio/PostPolicy.cs
@@ -0,0 +1,152 @@
+/*
+ * Minio .NET Library for Amazon S3 Compatible Cloud Storage, (C) 2015 Minio, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Minio
+{
+    public class PostPolicy
+    {
+            private DateTime expiration;
+            public List<Tuple<string, string, string>> policies =
+                    new List<Tuple<string, string, string>>();
+            public Dictionary<string, string> formData = new Dictionary<string, string>();
+            public string key { get; private set; }
+            public string bucket { get; private set; }
+
+            public void SetExpires(DateTime expiration)
+            {
+                    this.expiration = expiration;
+            }
+
+            public void SetKey(string Key)
+            {
+                    if (string.IsNullOrEmpty(Key))
+                    {
+                            throw new ArgumentException("Object key cannot be null or empty");
+                    }
+                    this.policies.Add(new Tuple<string, string, string>("eq", "$key", Key));
+                    this.formData.Add("key", Key);
+                    this.key = Key;
+            }
+
+            public void SetKeyStartsWith(string KeyStartsWith)
+            {
+                    if (string.IsNullOrEmpty(KeyStartsWith))
+                    {
+                            throw new ArgumentException("Object key cannot be null or empty");
+                    }
+                    this.policies.Add(new Tuple<string, string, string>("starts-with", "$key", KeyStartsWith));
+                    this.formData.Add("key", KeyStartsWith);
+            }
+
+            public void SetBucket(string Bucket)
+            {
+                    if (string.IsNullOrEmpty(Bucket))
+                    {
+                            throw new ArgumentException("Bucket key cannot be null or empty");
+                    }
+                    this.policies.Add(new Tuple<string, string, string>("eq", "$bucket", Bucket));
+                    this.formData.Add("bucket", Bucket);
+                    this.bucket = Bucket;
+            }
+
+            public void SetContentType(string ContentType)
+            {
+                    if (string.IsNullOrEmpty(ContentType))
+                    {
+                            throw new ArgumentException("Content-Type argument cannot be null or empty");
+                    }
+                    this.policies.Add(new Tuple<string, string, string>("eq", "$Content-Type", ContentType));
+                    this.formData.Add("Content-Type", ContentType);
+            }
+
+            private string marshalJSON()
+            {
+                    string returnStr;
+                    string expirationStr = "\"expiration\":\""+ this.expiration.ToString("yyyy-MM-ddTHH:mm:ss.000Z") + "\"";
+                    List<string> policies = new List<string>();
+                    foreach (var policy in this.policies)
+                    {
+                            policies.Add("[\"" + policy.Item1 + "\",\"" + policy.Item2 + "\",\"" + policy.Item3+"\"]");
+                    }
+                    StringBuilder policyFullStrBuilder = new StringBuilder();
+                    foreach (var policyStr in policies)
+                    {
+                            policyFullStrBuilder.Append(policyStr).Append(",");
+                    }
+                    string conditionsStr = "\"conditions\":["+ policyFullStrBuilder.ToString() + "]";
+
+                    returnStr = "{";
+                    if (expirationStr.Length > 0)
+                    {
+                            returnStr = returnStr + expirationStr + ",";
+                    }
+                    if (conditionsStr.Length > 0)
+                    {
+                            returnStr = returnStr + conditionsStr;
+                    }
+                    returnStr = returnStr + "}";
+                    Console.Out.WriteLine(returnStr);
+                    return returnStr;
+            }
+
+            public string Base64()
+            {
+                    string policyStr = this.marshalJSON();
+                    byte[] policyStrBytes = System.Text.Encoding.UTF8.GetBytes(policyStr as string);
+                    return Convert.ToBase64String(policyStrBytes);
+            }
+
+            public bool IsBucketSet()
+            {
+                    string value = "";
+                    if (this.formData.TryGetValue("bucket", out value))
+                    {
+                            if (!string.IsNullOrEmpty(value))
+                            {
+                                    return true;
+                            }
+                    }
+                    return false;
+            }
+
+            public bool IsKeySet()
+            {
+                    string value = "";
+                    if (this.formData.TryGetValue("key", out value))
+                    {
+                            if (!string.IsNullOrEmpty(value))
+                            {
+                                    return true;
+                            }
+                    }
+                    return false;
+            }
+
+            public bool IsExpirationSet()
+            {
+                    if (!string.IsNullOrEmpty(this.expiration.ToString()))
+                    {
+                            return true;
+                    }
+                    return false;
+            }
+    }
+}
