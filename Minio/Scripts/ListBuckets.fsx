1e1872f7e82ea1946589ecd2cada5bab27345340
Krishnan Parthasarathi <kp@minio.io>
2016-05-04 10:17:19 +0100

    Use minioClient instead of mc to bind MinioClient object
    Signed-off-by: Krishnan Parthasarathi <kp@minio.io>

--- Minio/Scripts/ListBuckets.fsx
+++ Minio/Scripts/ListBuckets.fsx
@@ -5,5 +5,5 @@ open Minio
 open System
 
 /// N B: Listing buckets on minio server at play.minio.io test access-key and secret-key
-let mc = new MinioClient("https://play.minio.io:9000", "Q3AM3UQ867SPQQA43P2F",  "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG")
-mc.ListBuckets() |> Seq.iter (fun b -> printfn "%s" b.Name)
+let minioClient = new MinioClient("https://play.minio.io:9000", "Q3AM3UQ867SPQQA43P2F",  "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG")
+minioClient.ListBuckets() |> Seq.iter (fun b -> printfn "%s" b.Name)
