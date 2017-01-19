a1cadc20d3584a534cb0b01e7a8210537982b4af
poornas <k.poorna@gmail.com>
2017-01-18 17:51:24 -0800

    Add bucketexistAsync and makebucketAsync calls.

--- Minio.Api/ApiEndpoints/IBucketOperations.cs
+++ Minio.Api/ApiEndpoints/IBucketOperations.cs
@@ -8,17 +8,16 @@ namespace Minio.Api
     public interface IBucketOperations
     {
         Task<ListAllMyBucketsResult> ListBucketsAsync();
-   /*
+  
         Task MakeBucketAsync(string bucketName, string location= "us-east-1");
 
         Task<bool> BucketExistsAsync(string bucketName);
-
+/*
         Task RemoveBucketAsync(string bucketName); //returns err in go-sdk <===
 
         Task<IEnumerable<Item>> ListObjectsAsync(string bucketName, string prefix,bool recursive);
 
-        Task<IEnumerable<Upload>> ListIncompleteUploadsAsync(string bucketName,string prefix,bool recursive);
-*/
+        Task<IEnumerable<Upload>> ListIncompleteUploadsAsync(string bucketName,string prefix,bool recursive);*/
 
     }
 }
