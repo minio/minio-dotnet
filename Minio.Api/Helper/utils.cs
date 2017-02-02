1c549ad4248f4a49526f63a5db0603d528be6b09
poornas <k.poorna@gmail.com>
2017-02-01 16:37:23 -0800

    add Apache License header for Minio Dotnet API

--- Minio.Api/Helper/utils.cs
+++ Minio.Api/Helper/utils.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/*
+ * Minio .NET Library for Amazon S3 Compatible Cloud Storage, (C) 2015 Minio, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +34,11 @@ namespace Minio
         // Invalid bucket name with double dot.
         static Regex invalidDotBucketName = new Regex("`/./.");
 
-        // isValidBucketName - verify bucket name in accordance with
-        //  - http://docs.aws.amazon.com/AmazonS3/latest/dev/UsingBucket.html
-
+        /// <summary>
+        /// isValidBucketName - verify bucket name in accordance with
+        ///  - http://docs.aws.amazon.com/AmazonS3/latest/dev/UsingBucket.html
+        /// </summary>
+        /// <param name="bucketName">Bucket to test existence of</param>
         internal static void validateBucketName(string bucketName)
         {
            if (bucketName.Trim() == "")
