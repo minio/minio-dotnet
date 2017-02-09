3b4386a4744cd7596e2c0ad516590987cab40954
Harshavardhana <harsha@minio.io>
2017-02-09 11:00:25 -0800

    Delete all older project files.

--- Minio/Scripts/ListBuckets.fsx
+++
@@ -1,9 +0,0 @@
-#I __SOURCE_DIRECTORY__
-#r "../bin/Release/Minio.dll"
-
-open Minio
-open System
-
-/// N B: Listing buckets on minio server at play.minio.io test access-key and secret-key
-let minioClient = new MinioClient("https://play.minio.io:9000", "Q3AM3UQ867SPQQA43P2F",  "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG")
-minioClient.ListBuckets() |> Seq.iter (fun b -> printfn "%s" b.Name)
