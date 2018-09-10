4161bf2ad57bc111a0cdf74cb27fb5e001e11c06
SanSYS <i.sansys@gmail.com>
2018-09-10 23:23:30 +0500

    Remove unnecessary Newtonsoft
    Allow to use custom tracers for request/responce
    
    The job exceeded the maximum log length, and has been terminated
    
    Restore MintLogger

---
+++ Minio.Functional.Tests/JsonNetLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Minio.DataModel.Tracing;
+using Newtonsoft.Json;
+
+namespace Minio.Functional.Tests
+{
+    internal class JsonNetLogger: IRequestLogger
+    {
+        public void LogRequest(RequestToLog requestToLog, ResponseToLog responseToLog, double durationMs)
+        {
+            Console.Out.WriteLine(string.Format("Request completed in {0} ms, Request: {1}, Response: {2}",
+                durationMs,
+                JsonConvert.SerializeObject(requestToLog, Formatting.Indented),
+                JsonConvert.SerializeObject(responseToLog, Formatting.Indented)));
+        }
+    }
+}
