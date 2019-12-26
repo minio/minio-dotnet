e5cf65de84820cbc7815cdc5489912d650ce9e8a
aevitas <m4rtijn@gmail.com>
2019-12-26 11:46:05 +0100

    Add x-amz-acl to supported headers

--- Minio/ApiEndpoints/ObjectOperations.cs
+++ Minio/ApiEndpoints/ObjectOperations.cs
@@ -34,7 +34,7 @@ namespace Minio
 {
     public partial class MinioClient : IObjectOperations
     {
-        List<string> supportedHeaders = new List<string> { "cache-control", "content-encoding", "content-type" };
+        List<string> supportedHeaders = new List<string> { "cache-control", "content-encoding", "content-type", "x-amz-acl" };
 
         /// <summary>
         /// Get an object. The object will be streamed to the callback given by the user.
