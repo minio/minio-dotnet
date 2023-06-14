e8f85ca74ff97c113fa7bc790a1afd210b7ec5a6
mytelegram <mytelegramserver@outlook.com>
2023-06-14 19:46:14 +0800

    ﻿perf:Replace dynamic with class for better performance Fix:NativeAOT build issue

---
+++ Minio/DataModel/MultiPartInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Minio.DataModel;
+
+public class MultiPartInfo
+{
+    public double PartSize { get; set; }
+    public double PartCount { get; set; }
+    public double LastPartSize { get; set; }
+}
