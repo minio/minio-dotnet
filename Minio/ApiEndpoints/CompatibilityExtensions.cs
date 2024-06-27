64398b7151743b40a8fe7575b1de5d00dce5ddf0
Ramon de Klein <mail@ramondeklein.nl>
2024-06-27 10:45:40 +0200

    Fix #1115

--- Minio/ApiEndpoints/CompatibilityExtensions.cs
+++ Minio/ApiEndpoints/CompatibilityExtensions.cs
@@ -10,11 +10,26 @@ public static class CompatibilityExtensions
     [Obsolete("Use the ListObjectsEnumAsync instead")]
     public static IObservable<Item> ListObjectsAsync(this IBucketOperations self, ListObjectsArgs args)
     {
+        return self.InternalListObjectsAsync(args, CancellationToken.None);
+    }
+
+    // Used for backwards compatibility (with added warning)
+    [Obsolete("Use the ListObjectsEnumAsync instead (also don't mix cancellation tokens and observables)")]
+    public static IObservable<Item> ListObjectsAsync(this IBucketOperations self, ListObjectsArgs args,
+        CancellationToken cancellationToken)
+    {
+        return self.InternalListObjectsAsync(args, cancellationToken);
+    }
+
+    private static IObservable<Item> InternalListObjectsAsync(this IBucketOperations self, ListObjectsArgs args,
+        CancellationToken cancellationToken)
+    {
         return Observable.Create<Item>(async (obs, ct) =>
         {
             try
             {
-                await foreach (var item in self.ListObjectsEnumAsync(args, ct).ConfigureAwait(false))
+                using var ctEffective = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, ct);
+                await foreach (var item in self.ListObjectsEnumAsync(args, ctEffective.Token).ConfigureAwait(false))
                     obs.OnNext(item);
                 obs.OnCompleted();
             }
@@ -26,14 +41,30 @@ public static class CompatibilityExtensions
     }
 
     // Used for backwards compatibility
-    [Obsolete("Use the ListIncompleteUploadsEnumAsync instead")]
+    [Obsolete("Use the ListObjectsEnumAsync instead")]
     public static IObservable<Upload> ListIncompleteUploads(this IObjectOperations self, ListIncompleteUploadsArgs args)
+    {
+        return self.InternalListIncompleteUploads(args, CancellationToken.None);
+    }
+
+    // Used for backwards compatibility (with added warning)
+    [Obsolete("Use the ListIncompleteUploads instead (also don't mix cancellation tokens and observables)")]
+    public static IObservable<Upload> ListIncompleteUploads(this IObjectOperations self, ListIncompleteUploadsArgs args,
+        CancellationToken cancellationToken)
+    {
+        return self.InternalListIncompleteUploads(args, cancellationToken);
+    }
+
+    private static IObservable<Upload> InternalListIncompleteUploads(this IObjectOperations self,
+        ListIncompleteUploadsArgs args, CancellationToken cancellationToken = default)
     {
         return Observable.Create<Upload>(async (obs, ct) =>
         {
             try
             {
-                await foreach (var item in self.ListIncompleteUploadsEnumAsync(args, ct).ConfigureAwait(false))
+                using var ctEffective = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, ct);
+                await foreach (var item in self.ListIncompleteUploadsEnumAsync(args, ctEffective.Token)
+                                   .ConfigureAwait(false))
                     obs.OnNext(item);
                 obs.OnCompleted();
             }
