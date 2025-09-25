ce7e408eded8190e96100df72b48b23b39f355a3
胡正 <zheng@sharecreators.com>
2025-09-25 16:18:35 +0800

    fix: Directly using user-defined endPoints is better than generating them automatically, as this code will result in invalid addresses set by the user
    # Conflicts:
    #	Minio/Helper/RequestUtil.cs

--- Minio/Helper/RequestUtil.cs
+++ Minio/Helper/RequestUtil.cs
@@ -51,8 +51,6 @@ internal static class RequestUtil
     {
         // For Amazon S3 endpoint, try to fetch location based endpoint.
         var host = endPoint;
-        if (S3utils.IsAmazonEndPoint(endPoint) && !string.IsNullOrEmpty(region))
-            host = $"s3.{region}.amazonaws.com";
 
         if (!usePathStyle)
         {
